2723992deb58830f47dd701bfd8af499b2660654
Lee-JuYeong <jane20021112@gmail.com>
2024-11-21 09:50:32 +0900

    Delete cosine-similality.py

--- Docker/cosine-similality.py
+++
@@ -1,7 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def calculate_similarity(data, text_column):
-    count_vectorizer = CountVectorizer()
-    vectors = count_vectorizer.fit_transform(data[text_column])
-    return cosine_similarity(vectors, vectors)
